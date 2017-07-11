ab43743a3951271f21cafd25f4ea292f96de8114
Rômulo Machado <romulo.machado5@gmail.com>
2017-07-11 14:11:30 -0700

    Use ember-cli-node-assets to import shed from node_modules

--- index.js
+++ index.js
@@ -4,9 +4,14 @@
 module.exports = {
   name: 'ember-ted-shed',
 
-  included: function(app) {
-    this._super.included.apply(this, arguments);
-
-    app.import('node_modules/shed-css/dist/index.css');
+  options: {
+    nodeAssets: {
+      'shed-css': function() {
+        return {
+          srcDir: 'dist',
+          import: ['index.css']
+        }
+      },
+    }
   }
 };
